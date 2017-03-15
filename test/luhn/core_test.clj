c3babbf3258594de0a3821ed1b641ca8d9556c89
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-15 01:35:54 +0100

    initial stub at a solution using repl-driven-developemnt

--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -1,3 +1,16 @@
 (ns luhn.core-test
   (:use midje.sweet)
   (:use [luhn.core]))
+
+(defn- sum-digits [n]
+  (apply + (map (comp read-string str) (str n))))
+
+(defn valid? [cc]
+  (let [reversed-cc (reverse cc)
+        odd-digits-sum (apply + (map (comp read-string str) (take-nth 2 reversed-cc)))
+        even-digits-calc (apply + (map sum-digits (map (comp #(* 2 %) (comp read-string str)) (take-nth 2 (rest reversed-cc)))))]
+    (= 0 (rem (+ odd-digits-sum even-digits-calc) 10))))
+
+(facts "about luhn test"
+       (fact "valid CC nums pass the test"
+             (valid? "49927398716") => true))
