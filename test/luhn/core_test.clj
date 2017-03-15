4fe6c7e75628361b4f999ed542f5ab2d0aeeaab1
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-15 12:22:09 +0100

    break composition in preparation to even-digits straction

--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -22,7 +22,7 @@
 (defn valid? [cc]
   (let [reversed-cc (reverse cc)
         odd-digits-sum (apply + (odd-digits reversed-cc))
-        even-digits-calc (apply + (map sum-digits (map (comp #(* 2 %) char->number) (evens reversed-cc))))]
+        even-digits-calc (apply + (map sum-digits (map #(* 2 %) (map char->number(evens reversed-cc)))))]
     (= 0 (rem (+ odd-digits-sum even-digits-calc) 10))))
 
 (facts "about luhn test"
