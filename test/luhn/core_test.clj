429ef571cf435d105f8da8a192e6fce6345dd377
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-15 12:10:49 +0100

    extract odds and evens functions

--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -5,10 +5,16 @@
 (defn- sum-digits [n]
   (apply + (map (comp read-string str) (str n))))
 
+(defn- odds [xs]
+  (take-nth 2 xs))
+
+(defn- evens [xs]
+  (take-nth 2 (rest xs)))
+
 (defn valid? [cc]
   (let [reversed-cc (reverse cc)
-        odd-digits-sum (apply + (map (comp read-string str) (take-nth 2 reversed-cc)))
-        even-digits-calc (apply + (map sum-digits (map (comp #(* 2 %) (comp read-string str)) (take-nth 2 (rest reversed-cc)))))]
+        odd-digits-sum (apply + (map (comp read-string str) (odds reversed-cc)))
+        even-digits-calc (apply + (map sum-digits (map (comp #(* 2 %) (comp read-string str)) (evens reversed-cc))))]
     (= 0 (rem (+ odd-digits-sum even-digits-calc) 10))))
 
 (facts "about luhn test"
