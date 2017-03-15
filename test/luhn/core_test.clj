612baf984be1c8b6c973a5f026e2a547421dc3e8
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-15 12:12:54 +0100

    add char->number function

--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -5,6 +5,9 @@
 (defn- sum-digits [n]
   (apply + (map (comp read-string str) (str n))))
 
+(defn- char->number [ch]
+  (read-string (str ch)))
+
 (defn- odds [xs]
   (take-nth 2 xs))
 
@@ -13,8 +16,8 @@
 
 (defn valid? [cc]
   (let [reversed-cc (reverse cc)
-        odd-digits-sum (apply + (map (comp read-string str) (odds reversed-cc)))
-        even-digits-calc (apply + (map sum-digits (map (comp #(* 2 %) (comp read-string str)) (evens reversed-cc))))]
+        odd-digits-sum (apply + (map char->number (odds reversed-cc)))
+        even-digits-calc (apply + (map sum-digits (map (comp #(* 2 %) char->number) (evens reversed-cc))))]
     (= 0 (rem (+ odd-digits-sum even-digits-calc) 10))))
 
 (facts "about luhn test"
